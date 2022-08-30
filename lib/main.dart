ce32e3a3779af7edfb43cbf4d9477966bacbe344
omaar1997 <31675083+omaar1997@users.noreply.github.com>
2022-08-30 14:05:33 +0200

    adding files

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'dart:async';
+
+import 'package:qr_code/screens/videoScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:video_player/video_player.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _scanBarcode = 'Unknown';
+  Future<void>? _launched;
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> scanQR() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.QR);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
+  // // Platform messages are asynchronous, so we initialize in an async method.
+  // Future<void> scanBarcodeNormal() async {
+  //   String barcodeScanRes;
+  //   // Platform messages may fail, so we use a try/catch PlatformException.
+  //   try {
+  //     barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+  //         '#ff6666', 'Cancel', true, ScanMode.BARCODE);
+  //     print(barcodeScanRes);
+  //   } on PlatformException {
+  //     barcodeScanRes = 'Failed to get platform version.';
+  //   }
+
+  //   // If the widget was removed from the tree while the asynchronous platform
+  //   // message was in flight, we want to discard the reply rather than calling
+  //   // setState to update our non-existent appearance.
+  //   if (!mounted) return;
+
+  //   setState(() {
+  //     _scanBarcode = barcodeScanRes;
+  //   });
+  // }
+
+  void checkQR() {
+    if (_scanBarcode == "https://github.com/deskbtm/android-adb-wlan") {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (_) => VideoScreen()));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Uri toLaunch = Uri.parse(_scanBarcode);
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(title: Center(child: const Text('Barcode scan'))),
+            body: Builder(builder: (BuildContext context) {
+              return Container(
+                  alignment: Alignment.center,
+                  child: Flex(
+                      direction: Axis.vertical,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        // ElevatedButton(
+                        //     onPressed: () => scanQR(),
+                        //     child: Text('Start QR scan')),
+                        ElevatedButton(
+                            onPressed: () => scanQR(),
+                            child: Text('Start QR scan')),
+                        ElevatedButton(
+                            onPressed: () => {
+                                  if (_scanBarcode ==
+                                      "https://github.com/deskbtm/android-adb-wlan")
+                                    {
+                                      Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                              builder: (_) => VideoScreen())),
+                                    }
+                                },
+                            child: Text('Watch The Video')),
+                        Text('Scan result : $_scanBarcode\n',
+                            style: TextStyle(fontSize: 20))
+                      ]));
+            })));
+  }
+}
---
+++ lib/screens/videoScreen.dart
@@ -0,0 +1,115 @@
+import 'dart:io';
+
+import 'package:chewie/chewie.dart';
+import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:video_player/video_player.dart';
+
+class VideoScreen extends StatefulWidget {
+  const VideoScreen({
+    Key? key,
+    this.title = 'Chewie Demo',
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _VideoScreenState();
+  }
+}
+
+class _VideoScreenState extends State<VideoScreen> {
+  TargetPlatform? _platform;
+  late VideoPlayerController _videoPlayerController1;
+  ChewieController? _chewieController;
+  int? bufferDelay;
+
+  @override
+  void initState() {
+    super.initState();
+    initializePlayer();
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController1.dispose();
+    _chewieController?.dispose();
+    super.dispose();
+  }
+
+  Future<void> initializePlayer() async {
+    _videoPlayerController1 = VideoPlayerController.asset("videos/om.mp4");
+    await Future.wait([
+      _videoPlayerController1.initialize(),
+    ]);
+    _createChewieController();
+    setState(() {});
+  }
+
+  void _createChewieController() {
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController1,
+      autoPlay: true,
+      looping: true,
+      progressIndicatorDelay:
+          bufferDelay != null ? Duration(milliseconds: bufferDelay!) : null,
+
+      hideControlsTimer: const Duration(seconds: 1),
+      allowPlaybackSpeedChanging: false,
+
+      // Try playing around with some of these other options:
+
+      //showControls: false,
+      // materialProgressColors: ChewieProgressColors(
+      //   playedColor: Colors.red,
+      //   handleColor: Colors.blue,
+      //   backgroundColor: Colors.grey,
+      //   bufferedColor: Colors.lightGreen,
+      // ),
+      // placeholder: Container(
+      //   color: Colors.grey,
+      // ),
+      // autoInitialize: true,
+    );
+  }
+
+  Future<void> toggleVideo() async {
+    await _videoPlayerController1.pause();
+    await initializePlayer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: widget.title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text(widget.title)),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                child: _chewieController != null &&
+                        _chewieController!
+                            .videoPlayerController.value.isInitialized
+                    ? Chewie(
+                        controller: _chewieController!,
+                      )
+                    : Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          CircularProgressIndicator(),
+                          SizedBox(height: 20),
+                          Text('Loading'),
+                        ],
+                      ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
